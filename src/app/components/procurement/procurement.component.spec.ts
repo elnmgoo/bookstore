69280c14f79a30ac21f2889f4f94c9fa793afaae
Mario Goorden <mario.goorden@gmail.com>
2021-01-17 11:14:00 +0100

    upgrading

--- src/app/components/procurement/procurement.component.spec.ts
+++ src/app/components/procurement/procurement.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ProcurementComponent } from './procurement.component';
 
@@ -6,7 +6,7 @@ describe('ProcurementComponent', () => {
   let component: ProcurementComponent;
   let fixture: ComponentFixture<ProcurementComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ProcurementComponent ]
     })
--- src/app/components/purchaseOrders/purchase-orders.component.spec.ts
+++ src/app/components/purchaseOrders/purchase-orders.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PurchaseOrdersComponent } from './purchase-orders.component';
 
@@ -6,7 +6,7 @@ describe('OrdersComponent', () => {
   let component: PurchaseOrdersComponent;
   let fixture: ComponentFixture<PurchaseOrdersComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PurchaseOrdersComponent ]
     })
