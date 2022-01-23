f612d659c05483662c9d27ae19f8f4deb75b5293
Mario Goorden <mario.goorden@gmail.com>
2022-01-23 13:27:40 +0100

    update date

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'http://127.0.0.1:8080/api',
   api2Url: 'http://127.0.0.1:8080/api2',
-  buildTimestamp: '07-01-2022'
+  buildTimestamp: '23-01-2022'
 };
